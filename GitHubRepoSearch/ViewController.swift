ce36883d0691466238e60df418843ade88a84342
Tim Hazhyi <morfey.ua@gmail.com>
2018-05-29 15:13:40 +0300

    add: viewController with tableView for request results

--- GitHubRepoSearch/ViewController.swift
+++ GitHubRepoSearch/ViewController.swift
@@ -2,14 +2,65 @@ import UIKit
 
 class ViewController: UIViewController {
     fileprivate var networkService: GraphQLService = GraphQLService()
+    fileprivate var tableView: UITableView!
+    fileprivate var repositories: [String] = [] {
+        didSet {
+            tableView?.reloadData()
+        }
+    }
+    
+    override func loadView() {
+        super.loadView()
+        loadStaticViews()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         networkService.getRepos(offset: 10, success: { [weak self] repos in
-            print(repos)
+            self?.repositories = repos.viewer.repositories.nodes?.map {$0?.name ?? ""} ?? []
         }, failure: {[weak self] error in
             print(error)
         })
     }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        if let cell_ = tableView.dequeueReusableCell(withIdentifier: "cell") as? UITableViewCell {
+            cell = cell_
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell.textLabel?.text = repositories[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories.count
+    }
+}
+
+extension ViewController {
+    func loadStaticViews() {
+        var cnts: [NSLayoutConstraint] = []
+        
+        tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        view.sendSubview(toBack: tableView)
+        
+        cnts += [tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)]
+        cnts += [tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)]
+        cnts += [tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)]
+        cnts += [tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)]
+        
+        NSLayoutConstraint.activate(cnts)
+    }
+}
